a9a8f73cccca8ab4f0841aaefa99339a45d1df99
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-03-09 20:48:09 +0000

    Adds packaging script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 metachains-dtc
 ==============
 
-Tool for accessing and dumping metachain data into the Datacoin blockcain.
+Tool for accessing and dumping metachain data into the Datacoin blockchain.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='metachains_dtc',
+    version='0.1.0',
+    author='Hugo Peixoto',
+    author_email='hugo.peixoto@gmail.com',
+    packages=['metachains_dtc'],
+    scripts=[],
+    url='https://github.com/Storj/metachains_dtc',
+    license='LICENSE',
+    description='Tool for accessing and dumping metadata into the Datacoin blockchain',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'requests >= 0.11.0',
+    ],
+)
