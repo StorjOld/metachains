c47ef61c22d2f1ab190dd6fc34b6d592ec927415
Josh Brandoff <EmergentBehavior@users.noreply.github.com>
2014-11-13 02:12:30 -0500

    Remove dtc references

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='metachains_dtc',
+    name='metachains',
     version='0.4.0',
     author='Hugo Peixoto',
     author_email='hugo.peixoto@gmail.com',
     packages=find_packages(),
     scripts=[],
-    url='https://github.com/Storj/metachains_dtc',
+    url='https://github.com/Storj/metachains',
     license='LICENSE',
     description='Tool for accessing and dumping metadata into the Florincoin blockchain',
     long_description=open('README.md').read(),
